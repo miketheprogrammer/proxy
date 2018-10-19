82c1a96a825aff6e18b551bcdab09d8c675ad495
Michael Hernandez <michael.hernandez1988@gmail.com>
2018-10-18 20:11:25 -0400

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const httpProxy = require('http-proxy');
+const options = {}
+const proxy = httpProxy.createProxyServer(options);
+
+const server = http.createServer(function(req, res) {
+  proxy.web(req, res, { target: 'http://localhost:8080' });
+});
+
+proxy.on('error', function(e) {
+  console.log('Proxy Error', e);
+});
+
+server.listen(80);
